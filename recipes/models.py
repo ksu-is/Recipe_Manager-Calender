c0c9677c338c5b5ce715911807a6dbfa91990097
bdilbeck <bdilbec4@students.kennesaw.edu>
2021-12-05 15:12:29 -0500

    Added recipe description

--- recipes/models.py
+++ recipes/models.py
@@ -21,6 +21,7 @@ class Recipe(models.Model):
     source = models.ForeignKey(RecipeSource, blank=True, null=True, on_delete=models.SET_NULL)
     source_hint = models.CharField(max_length=50, blank=True)
     ingredients = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=500, blank=True)
     
     def __str__(self):
         return self.name
